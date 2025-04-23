4f3298fcb589136b33d0575a0ee11dd2d086103b
Prashant3002 <singh.prashant3002@gmail.com>
2025-04-23 23:49:06 +0530

    Auto-commit: 2025-04-23 23:49:06

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,22 +1,30 @@
-import NextAuth from "next-auth";
+import { DefaultSession, DefaultUser } from "next-auth";
+import { JWT } from "next-auth/jwt";
 
 declare module "next-auth" {
-  interface User {
-    id: string;
-    role: string;
-  }
-  
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
   interface Session {
-    user: User & {
+    accessToken?: string;
+    user: {
       id: string;
       role: string;
-    };
+    } & DefaultSession["user"];
+  }
+  
+  interface User extends DefaultUser {
+    id: string;
+    role: string;
+    token?: string;
   }
 }
 
 declare module "next-auth/jwt" {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
   interface JWT {
     id: string;
     role: string;
+    accessToken?: string;
   }
 } 
